4846c3ff9e3ec8d1cc8cf2bc054ede121195578a
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2024-02-27 15:48:30 +0100

    fix cors

--- main.go
+++ main.go
@@ -44,7 +44,7 @@ func main() {
 	router := chi.NewRouter()
 
 	router.Use(cors.Handler(cors.Options{
-		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		// AllowedOrigins:   []string{"https://*"}, // Use this to allow specific origin hosts
 		AllowedOrigins: []string{"https://*", "http://*"},
 		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
