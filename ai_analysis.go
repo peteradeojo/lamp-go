0d1586f888b59d1661a504ecfbb9ed9de8f8815a
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2024-05-08 23:30:12 +0100

    use env to provide the ai hostname

--- ai_analysis.go
+++ ai_analysis.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 
@@ -50,6 +51,11 @@ func CleanMessage(txt string) string {
 }
 
 func (apiCfg *ApiConfig) RunAIAnalysis(w http.ResponseWriter, r *http.Request) {
+	OLLAMA_HOST := os.Getenv("OLLAMA_HOST")
+	if OLLAMA_HOST == "" {
+		handlers.RespondWithError(w, 500, "Model host not configured")
+		return
+	}
 	logId, err := strconv.Atoi(chi.URLParam(r, "logId"))
 	if err != nil {
 		handlers.SaveError(apiCfg.DB, r, err)
@@ -68,7 +74,7 @@ func (apiCfg *ApiConfig) RunAIAnalysis(w http.ResponseWriter, r *http.Request) {
 
 	var reader = strings.NewReader(fmt.Sprintf(`{"messages": [{"role": "system", "content": "You are to act as an analyser for log messages. You will be provided with a generated log message from a running application. The message might contain context provided as a JSON string truncated to it's first 50 characters. You will attempt to determine the programming language from the log message and provided context and attempt to provide a description about the issue to be diagnosed and provide pointers on how best to resolve the issue."}, {"role": "user", "content": "%v"}], "stream": false, "model": "llama3"}`, content))
 
-	resp, err := http.Post("http://localhost:11434/api/chat", "text/json", reader)
+	resp, err := http.Post(fmt.Sprintf("%s/api/chat", OLLAMA_HOST), "text/json", reader)
 
 	if err != nil {
 		llog.Println(err)
@@ -79,7 +85,6 @@ func (apiCfg *ApiConfig) RunAIAnalysis(w http.ResponseWriter, r *http.Request) {
 	defer resp.Body.Close()
 
 	i, err := io.ReadAll(resp.Body)
-	fmt.Println(string(i))
 	if err != nil {
 		handlers.SaveError(apiCfg.DB, r, err)
 		llog.Println(err)
